88a248cf25a7f1e7a3f2df2441645620799bf875
betabeta0608 <betabeta0608@yahoo.co.jp>
2020-05-21 10:37:38 +0900

    使用していなかった関数を削除

--- analysis.py
+++ analysis.py
@@ -24,23 +24,3 @@ def summary_regression(config):
     df_summary = pd.concat(dfs)
     df_summary.to_csv(os.path.join(result_root, "miss_summary.csv"), index=False)
     return stats.pearsonr(df_summary["true"], df_summary["predict"])[0]
-
-# 引っ張ってきただけ
-def summary_analysis_regression(miss_summary_file, summary_file, fig_file):
-    df = pd.read_csv(miss_summary_file, encoding="utf-8")
-    y_true = df["true"]
-    y_pred = df["predict"]
-    r, p = stats.pearsonr(y_true, y_pred)
-    print("相関係数")
-    print(r, p)
-    df_out = pd.DataFrame()
-    df_out["pearsonr"] = r, p
-    df_out.to_csv(summary_file, index=False, encoding="utf-8")
-
-    plt.figure()
-    plt.scatter(y_true, y_pred)
-    plt.xlabel('Ground value')
-    plt.ylabel('Predict Value')
-    plt.title('Prediction')
-    plt.savefig(fig_file)
-    plt.close()
